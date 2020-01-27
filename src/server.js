0c9d0b6d5cafbffc5a6c7095514957488a07df29
Otto F. Quintanilha <ottofq@gmail.com>
2020-01-27 18:06:37 -0300

    add sitaxe de classe

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 require("dotenv").config();
-const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
+const app = require("./app");
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
-mongoose.set("useCreateIndex", true);
-
-app.use(express.json());
-app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Rodando na porta ${process.env.PORT}`);
