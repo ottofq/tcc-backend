8c47c737cd6896c16716d95fe679a9b3ab586afb
Otto F. Quintanilha <ottofq@gmail.com>
2020-05-22 22:51:37 -0300

    refatora paginacao de informacoes

--- src/controllers/informacaoController.js
+++ src/controllers/informacaoController.js
@@ -14,15 +14,16 @@ class InfoController {
 
   async readAll(req, res) {
     const { page } = req.query;
-    const skips = 10 * (page - 1);
+    const skips = 8 * (page - 1);
 
     try {
+      const total_infos = await infoModel.countDocuments();
       const result = await infoModel
         .find()
         .skip(skips)
-        .limit(10)
+        .limit(8)
         .sort({ _id: -1 });
-      return res.json(result);
+      return res.json({ total_infos, result });
     } catch (error) {
       return res.status(400).json(error);
     }
