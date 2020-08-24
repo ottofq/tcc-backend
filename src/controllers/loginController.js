25cb8690709fac3e339b3d85b758ac807a0a21cf
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-24 17:53:39 -0300

    refactor login controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,33 +1,15 @@
-const jwt = require('jsonwebtoken');
-const userModel = require('../models/userModel');
-const passwordUtil = require('../utils/passwordUtils');
+const createLoginService = require('../services/loginServices/createLoginService');
 
 class Login {
   async create(req, res) {
     const { email, password } = req.body;
 
-    const user = await userModel.findOne({ email }).select('+hash_password');
-
-    if (!user) {
-      return res.status(400).json({ error: 'usuário não encontrado!' });
+    try {
+      const user = await createLoginService.handle(email, password);
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(401).json({ error: error.message });
     }
-
-    if (!(await passwordUtil.comparePassword(password, user.hash_password))) {
-      return res.status(400).json({ error: 'senha incorreta!' });
-    }
-
-    const { _id, nome } = user;
-
-    return res.json({
-      user: {
-        _id,
-        nome,
-        email,
-      },
-      token: jwt.sign({ _id }, process.env.JWT_SECRET, {
-        expiresIn: '7d',
-      }),
-    });
   }
 }
 
