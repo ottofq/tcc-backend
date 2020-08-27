ef48a8de9df6aee5dd957d2fe2a11b8ab4b03bed
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-26 21:59:41 -0300

    add error handling in create student service

--- src/services/studentServices/createStudentService.js
+++ src/services/studentServices/createStudentService.js
@@ -1,10 +1,27 @@
 const studentRespository = require('../../repositories/studentRepository');
+const BadRequestError = require('../../utils/errors/badRequestError');
+const InternalServerError = require('../../utils/errors/internalServerError');
 
 class CreateStudentService {
   async handle(student) {
-    const menuCreated = await studentRespository.create(student);
+    try {
+      const studentExists = await studentRespository.findByRegistration(
+        student.matricula
+      );
 
-    return menuCreated;
+      if (studentExists) {
+        throw Error('Já existe um aluno associado a essa matricula');
+      }
+
+      const studentCreated = await studentRespository.create(student);
+
+      return studentCreated;
+    } catch (error) {
+      if (error.name === 'DBError') {
+        throw new InternalServerError('Internal Server Error');
+      }
+      throw new BadRequestError(error.message);
+    }
   }
 }
 
