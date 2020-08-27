5f18f251e402021dc3b152de936a779f7bdc9c8f
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-27 16:52:51 -0300

    add errors handling in lists students

--- src/services/studentServices/listStudentsService.js
+++ src/services/studentServices/listStudentsService.js
@@ -1,11 +1,16 @@
 const studentRespository = require('../../repositories/studentRepository');
+const InternalServerError = require('../../utils/errors/internalServerError');
 
 class ListStudentsService {
   async handle(skip, limit) {
-    const students = await studentRespository.list(skip, limit);
-    const total_students = await studentRespository.countStudents();
+    try {
+      const students = await studentRespository.lista(skip, limit);
+      const total_students = await studentRespository.countStudents();
 
-    return { total_students, students };
+      return { total_students, students };
+    } catch (error) {
+      throw new InternalServerError('Internal Server Error');
+    }
   }
 }
 
