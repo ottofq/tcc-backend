5d66efdcc3a373d142b97fe332da35116ad23393
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-31 18:22:15 -0300

    add validator middleware in news routes

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -2,15 +2,21 @@ const { Router } = require('express');
 
 const newsController = require('../controllers/newsController');
 const auth = require('../middlewares/authMiddleware');
+const { idValidator, newsValidator } = require('../middlewares/validators');
 
 const newsRoutes = Router();
 
 newsRoutes.use(auth);
 
-newsRoutes.post('/', newsController.create);
-newsRoutes.get('/', newsController.list);
-newsRoutes.get('/:id', newsController.find);
-newsRoutes.put('/:id', newsController.update);
-newsRoutes.delete('/:id', newsController.delete);
+newsRoutes.post('/', newsValidator.create, newsController.create);
+newsRoutes.get('/', newsValidator.list, newsController.list);
+newsRoutes.get('/:id', idValidator, newsController.find);
+newsRoutes.put(
+  '/:id',
+  idValidator,
+  newsValidator.update,
+  newsController.update
+);
+newsRoutes.delete('/:id', idValidator, newsController.delete);
 
 module.exports = newsRoutes;
