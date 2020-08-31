892d1e904c44790e413a935b28af7dd709acbf75
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-31 18:21:52 -0300

    add validator middleware in users routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,12 +2,19 @@ const { Router } = require('express');
 
 const userController = require('../controllers/userController');
 const auth = require('../middlewares/authMiddleware');
+const { userValidator, idValidator } = require('../middlewares/validators');
 
 const usersRoutes = Router();
 
-usersRoutes.post('/', userController.create);
-usersRoutes.get('/', auth, userController.list);
-usersRoutes.put('/:id', auth, userController.update);
-usersRoutes.delete('/:id', auth, userController.delete);
+usersRoutes.post('/', userValidator.create, userController.create);
+usersRoutes.get('/', auth, userValidator.list, userController.list);
+usersRoutes.put(
+  '/:id',
+  idValidator,
+  userValidator.update,
+  auth,
+  userController.update
+);
+usersRoutes.delete('/:id', idValidator, auth, userController.delete);
 
 module.exports = usersRoutes;
