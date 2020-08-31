06463992c89c053f3394262c0dc97cd7ccb46608
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-31 18:22:07 -0300

    add validator middleware in menu routes

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -3,19 +3,35 @@ const { Router } = require('express');
 const menuController = require('../controllers/menuController');
 const ratingController = require('../controllers/ratingController');
 const auth = require('../middlewares/authMiddleware');
+const { menuValidator, idValidator } = require('../middlewares/validators');
 
 const menuRoutes = Router();
 
 menuRoutes.use(auth);
 
-menuRoutes.get('/', menuController.readAll);
+menuRoutes.get('/', menuValidator.listAllMenus, menuController.readAll);
 menuRoutes.get('/last', menuController.readLast);
-menuRoutes.get('/:id', menuController.readOne);
-menuRoutes.get('/:id/comentarios', ratingController.listComments);
-menuRoutes.get('/avg/:id', ratingController.average);
-menuRoutes.post('/', menuController.create);
-menuRoutes.post('/avaliar/:id', ratingController.create);
-menuRoutes.put('/:id', menuController.update);
-menuRoutes.delete('/:id', menuController.delete);
+menuRoutes.get('/:id', idValidator, menuController.readOne);
+menuRoutes.get(
+  '/:id/comentarios',
+  idValidator,
+  menuValidator.listComments,
+  ratingController.listComments
+);
+menuRoutes.get('/avg/:id', idValidator, ratingController.average);
+menuRoutes.post('/', menuValidator.create, menuController.create);
+menuRoutes.post(
+  '/avaliar/:id',
+  idValidator,
+  menuValidator.createRating,
+  ratingController.create
+);
+menuRoutes.put(
+  '/:id',
+  idValidator,
+  menuValidator.update,
+  menuController.update
+);
+menuRoutes.delete('/:id', idValidator, menuController.delete);
 
 module.exports = menuRoutes;
