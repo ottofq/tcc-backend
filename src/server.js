cbcd5751c34e48fdf29c2474ef6d62809ae8d24c
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-02 18:08:16 -0300

    add celebrate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 require('./database/mongo');
 require('./database/redis');
+const { errors } = require('celebrate');
 
 const app = require('./app');
 
+app.use(errors());
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Rodando na porta ${process.env.PORT}`);
+  console.log(`🏃 Running in port ${process.env.PORT}`);
 });
