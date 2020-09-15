1c6b77dac42cb64565f8734e2a919463fed064c7
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-14 23:04:27 -0300

    add create app login in login controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,5 @@
 const createLoginService = require('../services/loginServices/createLoginService');
+const createAppLoginService = require('../services/loginServices/createAppLoginService');
 
 class Login {
   async create(req, res) {
@@ -6,6 +7,17 @@ class Login {
 
     try {
       const user = await createLoginService.handle(email, password);
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(401).json({ error: error.message });
+    }
+  }
+
+  async createAppLogin(req, res) {
+    const { email, password } = req.body;
+
+    try {
+      const user = await createAppLoginService.handle(email, password);
       return res.status(200).json(user);
     } catch (error) {
       return res.status(401).json({ error: error.message });
