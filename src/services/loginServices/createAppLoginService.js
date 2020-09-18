2c65bb3a0a92a698a0a30a2a39774b15501f1120
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-17 23:33:06 -0300

    refactor error handling in create login service

--- src/services/loginServices/createAppLoginService.js
+++ src/services/loginServices/createAppLoginService.js
@@ -1,36 +1,34 @@
 const jwt = require('jsonwebtoken');
 const studentRepository = require('../../repositories/studentRepository');
 const passwordUtil = require('../../utils/passwordUtils');
+const ForbiddenError = require('../../utils/errors/forbiddenError');
+const NotFoundError = require('../../utils/errors/notFoundError');
 
 class CreateAppLoginService {
   async handle(email, password) {
-    try {
-      const studentExists = await studentRepository.findByEmailPass(email);
+    const studentExists = await studentRepository.findByEmailPass(email);
 
-      if (!studentExists) {
-        throw Error('Usuário não existente na base de dados!');
-      }
-
-      const verifyPassword = await passwordUtil.comparePassword(
-        password,
-        studentExists.hash_password
-      );
-
-      if (!verifyPassword) {
-        throw Error('Email/password incorreto!');
-      }
-      const { _id } = studentExists;
-
-      const token = jwt.sign({ _id }, process.env.JWT_SECRET, {
-        expiresIn: '7d',
-      });
-
-      studentExists.hash_password = undefined;
-
-      return { student: studentExists, auth: { token } };
-    } catch (error) {
-      throw new Error(error.message);
+    if (!studentExists) {
+      throw new NotFoundError('Usuário não existente na base de dados!');
     }
+
+    const verifyPassword = await passwordUtil.comparePassword(
+      password,
+      studentExists.hash_password
+    );
+
+    if (!verifyPassword) {
+      throw new ForbiddenError('Email/password incorreto!');
+    }
+    const { _id } = studentExists;
+
+    const token = jwt.sign({ _id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    studentExists.hash_password = undefined;
+
+    return { student: studentExists, auth: { token } };
   }
 }
 
--- src/services/loginServices/createLoginService.js
+++ src/services/loginServices/createLoginService.js
@@ -1,40 +1,38 @@
 const jwt = require('jsonwebtoken');
 const userRepository = require('../../repositories/userRepository');
 const passwordUtil = require('../../utils/passwordUtils');
+const ForbiddenError = require('../../utils/errors/forbiddenError');
+const NotFoundError = require('../../utils/errors/notFoundError');
 
 class CreateLoginService {
   async handle(email, password) {
-    try {
-      const userExists = await userRepository.findEmail(email);
+    const userExists = await userRepository.findEmail(email);
 
-      if (!userExists) {
-        throw Error('Usuário não existente na base de dados!');
-      }
-
-      const verifyPassword = await passwordUtil.comparePassword(
-        password,
-        userExists.hash_password
-      );
-
-      if (!verifyPassword) {
-        throw Error('Email/password incorreto!');
-      }
-
-      const { _id, nome } = userExists;
-
-      return {
-        user: {
-          _id,
-          nome,
-          email,
-        },
-        token: jwt.sign({ _id }, process.env.JWT_SECRET, {
-          expiresIn: '7d',
-        }),
-      };
-    } catch (error) {
-      throw new Error(error.message);
+    if (!userExists) {
+      throw new NotFoundError('Usuário não existente na base de dados!');
     }
+
+    const verifyPassword = await passwordUtil.comparePassword(
+      password,
+      userExists.hash_password
+    );
+
+    if (!verifyPassword) {
+      throw new ForbiddenError('Email/password incorreto!');
+    }
+
+    const { _id, nome } = userExists;
+
+    return {
+      user: {
+        _id,
+        nome,
+        email,
+      },
+      token: jwt.sign({ _id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      }),
+    };
   }
 }
 
