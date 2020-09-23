541b6052f788ce8912bcc731d55b83713f73dfcc
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-23 20:27:06 -0300

    add update student in controller

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -4,6 +4,7 @@ const {
   findStudentService,
   findByRegistrationStudentService,
   findEmailStudentService,
+  updateStudentService,
 } = require('../services/studentServices');
 
 class StudentController {
@@ -23,7 +24,6 @@ class StudentController {
       nivel_fisico,
       peso_ideal,
       alergias: {
-        nenhuma,
         alergia_gluten,
         intolerancia_lactose,
         proteina_leite_vaca,
@@ -33,7 +33,6 @@ class StudentController {
       adiciona_sal,
       utiliza_oleo_composto,
       consome_bebida_alcoolica,
-      frequencia_alcool,
       tabagista,
       patologias: {
         doenca_cardiovascular,
@@ -87,7 +86,6 @@ class StudentController {
       nivel_fisico,
       peso_ideal,
       alergias: {
-        nenhuma,
         alergia_gluten,
         intolerancia_lactose,
         proteina_leite_vaca,
@@ -97,7 +95,6 @@ class StudentController {
       adiciona_sal,
       utiliza_oleo_composto,
       consome_bebida_alcoolica,
-      frequencia_alcool,
       tabagista,
       patologias: {
         doenca_cardiovascular,
@@ -190,6 +187,136 @@ class StudentController {
     try {
       const student = await findByRegistrationStudentService.handle(matricula);
       return res.status(200).json(student);
+    } catch (error) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const {
+        _id,
+        nome,
+        data_nascimento,
+        curso,
+        ano_ingresso,
+        sexo,
+        bolsista,
+        frequencia_RU,
+        tipo_refeicao_RU,
+        nivel_fisico,
+        peso_ideal,
+        alergias: {
+          alergia_gluten,
+          intolerancia_lactose,
+          proteina_leite_vaca,
+          outras_alergias,
+        },
+        vegano_vegetariano,
+        adiciona_sal,
+        utiliza_oleo_composto,
+        consome_bebida_alcoolica,
+        tabagista,
+        patologias: {
+          doenca_cardiovascular,
+          hipertensao_arterial,
+          obesidade,
+          dislipidemias,
+          doenca_arterial_coronariana,
+          diabetes,
+          outras_patologias,
+        },
+        patologias_familia: {
+          fam_doenca_cardiovascular,
+          fam_hipertensao,
+          fam_obesidade,
+          fam_dislipidemias,
+          fam_doenca_arterial_coronariana,
+          fam_diabetes,
+          patologias_familia_outras,
+        },
+        medicamento_continuo,
+        avaliacao_RU: {
+          aroma,
+          coloracao_cardapio,
+          textura_preparacao,
+          sabor_preparacao,
+          avaliacao_geral,
+        },
+        melhorias_RU: {
+          cardapio,
+          melhoria_sabor_preparacao,
+          opcao_vegetariana,
+          estrutura_fisica,
+          tempo_fila,
+          preco_ticket,
+          melhoria_outros,
+        },
+      } = req.body;
+
+      const student = {
+        _id,
+        nome,
+        data_nascimento,
+        curso,
+        ano_ingresso,
+        sexo,
+        bolsista,
+        frequencia_RU,
+        tipo_refeicao_RU,
+        nivel_fisico,
+        peso_ideal,
+        alergias: {
+          alergia_gluten,
+          intolerancia_lactose,
+          proteina_leite_vaca,
+          outras_alergias,
+        },
+        vegano_vegetariano,
+        adiciona_sal,
+        utiliza_oleo_composto,
+        consome_bebida_alcoolica,
+        tabagista,
+        patologias: {
+          doenca_cardiovascular,
+          hipertensao_arterial,
+          obesidade,
+          dislipidemias,
+          doenca_arterial_coronariana,
+          diabetes,
+          outras_patologias,
+        },
+        patologias_familia: {
+          fam_doenca_cardiovascular,
+          fam_hipertensao,
+          fam_obesidade,
+          fam_dislipidemias,
+          fam_doenca_arterial_coronariana,
+          fam_diabetes,
+          patologias_familia_outras,
+        },
+        medicamento_continuo,
+        avaliacao_RU: {
+          aroma,
+          coloracao_cardapio,
+          textura_preparacao,
+          sabor_preparacao,
+          avaliacao_geral,
+        },
+        melhorias_RU: {
+          cardapio,
+          melhoria_sabor_preparacao,
+          opcao_vegetariana,
+          estrutura_fisica,
+          tempo_fila,
+          preco_ticket,
+          melhoria_outros,
+        },
+      };
+
+      const studentUpdated = await updateStudentService.handle(student);
+
+      return res.status(200).json({ student: studentUpdated });
     } catch (error) {
       return res.status(error.statusCode).json({ error: error.message });
     }
