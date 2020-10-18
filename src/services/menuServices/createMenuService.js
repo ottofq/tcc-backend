c7d815dcffbe16926b920510e3c5b94e20a172f6
Otto F. Quintanilha <ottofq@gmail.com>
2020-10-17 23:16:42 -0300

    remove create rating collection in create menu service

--- src/services/menuServices/createMenuService.js
+++ src/services/menuServices/createMenuService.js
@@ -1,12 +1,11 @@
 const menuRepository = require('../../repositories/menuRepository');
-const createRatingCollectionService = require('../ratingServices/createRatingColletionService');
+
 const InternalServerError = require('../../utils/errors/internalServerError');
 
 class CreateMenuService {
   async handle(menu) {
     try {
       const menuCreated = await menuRepository.create(menu);
-      await createRatingCollectionService.handle(menuCreated._id);
 
       return menuCreated;
     } catch (error) {
