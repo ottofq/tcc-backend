52333a30eccfbca52e1e5a82fd6369f03802777f
Otto F. Quintanilha <ottofq@gmail.com>
2023-03-19 16:02:47 -0300

    feat: remove auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,21 +2,22 @@ const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 
 module.exports = async (req, res, next) => {
-  const auth = req.headers.authorization;
-
-  if (!auth) {
-    return res.status(401).json({ error: 'Token não enviado!' });
-  }
-
-  const [, token] = auth.split(' ');
-
-  try {
-    await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    return next();
-  } catch (error) {
-    return res
-      .status(401)
-      .json({ error: error.name, statusCode: 401, message: error.message });
-  }
+  // const auth = req.headers.authorization;
+  //
+  // if (!auth) {
+  //   return res.status(401).json({ error: 'Token não enviado!' });
+  // }
+  //
+  // const [, token] = auth.split(' ');
+  //
+  // try {
+  //   await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  //
+  //   return next();
+  // } catch (error) {
+  //   return res
+  //     .status(401)
+  //     .json({ error: error.name, statusCode: 401, message: error.message });
+  // }
+  return next();
 };
